750cbb236cc965b14541bef0692012b25b417d8a
Ray <Ray@PC310>
2025-08-18 12:11:51 +0800

    更新語法-移除手續費結案條件

--- Dev_kcsddb2/kcsd/Script/TempReport/機車部全產品別有佣金案件名單.sql
+++ Dev_kcsddb2/kcsd/Script/TempReport/機車部全產品別有佣金案件名單.sql
@@ -1,6 +1,7 @@
 ﻿--25.08.15 提供v1(2025.05.01~2025.07.31)   C:\Users\Ray\Documents\Ray\臨時報表\財管1140815-機車部全產品別有佣金之案件 v1.xls 
 --25.08.15 提供v2(2025.01.01~2025.07.31)   C:\Users\Ray\Documents\Ray\臨時報表\財管1140815-機車部全產品別有佣金之案件 v2.xls 
-DECLARE @wk_strt_date Date ='2025-07-01',
+--25.08.18 移除手續費結案條件, 且提供v3(2025.05.01~2025.07.31)   C:\Users\Ray\Documents\Ray\臨時報表\財管1140815-機車部全產品別有佣金之案件 v3.xls 
+DECLARE @wk_strt_date Date ='2025-05-01',
                    @wk_stop_date Date ='2025-07-31'
 SELECT C.kc_area_code + ' ' + A.kc_area_desc'分公司', CONVERT(Date, C.kc_buy_date)'建檔日', CP.kc_prod_type + ' ' + SC.[Text]'產品別', C.kc_cp_no'CP編號', 
 			   C.kc_case_no'客戶編號', C.kc_cust_nameu'客戶姓名', C.kc_id_no'身分證號', 
@@ -9,7 +10,6 @@ SELECT C.kc_area_code + ' ' + A.kc_area_desc'分公司', CONVERT(Date, C.kc_buy_
 			   C.kc_brok_fee2'佣金', C.kc_loan_perd'期數', C.kc_perd_fee'月付款', kc_cons_rate'CP利率'
 FROM kcsd.kc_customerloan C
 INNER JOIN kcsd.kc_cpdata CP ON C.kc_cp_no=CP.kc_cp_no
-INNER JOIN kcsd.kc_loanpayment  P ON C.kc_case_no= P.kc_case_no AND P.kc_intr_fee IS NOT NULL  AND P.kc_intr_fee>0
 INNER JOIN kcsd.kct_area A ON C.kc_area_code=A.kc_area_code
 INNER JOIN [Zephyr.Sys].dbo.sys_code SC ON SC.CodeType='ProductType' AND C.kc_prod_type=SC.[Value]
 INNER JOIN kcsd.kc_caragent AG ON C.kc_comp_code = AG.kc_agent_code
