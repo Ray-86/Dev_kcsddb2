3218d20c173821b1dd14dd68efe34f72b56fc06d
Ray <Ray@PC310>
2025-08-19 12:30:50 +0800

    修復Index1020查詢問題

--- Dev_kcsddb2/kcsd/Script/BugFix/Index1020.sql
+++ Dev_kcsddb2/kcsd/Script/BugFix/Index1020.sql
@@ -1,4 +1,5 @@
-﻿DECLARE @wk_strt_date Date, @wk_stop_date Date,
+﻿-- 25.08.19 kc_audit_date 資料型態是dateTime, 查詢是轉成Date
+DECLARE @wk_strt_date Date, @wk_stop_date Date,
 				   @wk_strt_date2 Date, @wk_stop_date2 Date,
 				   @wk_area_code varchar(5),
 				   @wk_biz_stat varchar(5),
@@ -9,7 +10,7 @@ FROM kcsd.kc_caragent C
 LEFT JOIN kcsd.kc_caragentbranchProd P 
 	   ON C.kc_agent_code = P.kc_agent_code
 WHERE 1=1
-  AND ((@wk_strt_date='' OR @wk_stop_date='') OR (kc_audit_date BETWEEN @wk_strt_date AND @wk_stop_date))  --審核日期
+  AND ((@wk_strt_date='' OR @wk_stop_date='') OR (CONVERT(Date, kc_audit_date) BETWEEN @wk_strt_date AND @wk_stop_date))  --審核日期
   AND ((@wk_strt_date2='' OR @wk_stop_date2='') OR (kc_dev_date BETWEEN @wk_strt_date2 AND @wk_stop_date2))  --開發日期
   AND (@wk_area_code = '' OR kc_area_code = @wk_area_code) --分公司
   AND (@wk_biz_stat = '' OR kc_biz_stat = @wk_biz_stat) --登記現況
