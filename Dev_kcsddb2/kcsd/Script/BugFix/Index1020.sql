c886a8d0fbb0eaf0496380bcf914adfaf5dbbf0b
Ray <Ray@PC310>
2025-08-19 12:25:58 +0800

    初版語法, 問題 : 審核日期篩選結果少於實際結果

---
+++ Dev_kcsddb2/kcsd/Script/BugFix/Index1020.sql
@@ -0,0 +1,16 @@
+﻿DECLARE @wk_strt_date Date, @wk_stop_date Date,
+				   @wk_strt_date2 Date, @wk_stop_date2 Date,
+				   @wk_area_code varchar(5),
+				   @wk_biz_stat varchar(5),
+				   @wk_sales_code2 varchar(5)
+
+SELECT DISTINCT p.kc_sales_code'new_sales_code', c.*
+FROM kcsd.kc_caragent C
+LEFT JOIN kcsd.kc_caragentbranchProd P 
+	   ON C.kc_agent_code = P.kc_agent_code
+WHERE 1=1
+  AND ((@wk_strt_date='' OR @wk_stop_date='') OR (kc_audit_date BETWEEN @wk_strt_date AND @wk_stop_date))  --審核日期
+  AND ((@wk_strt_date2='' OR @wk_stop_date2='') OR (kc_dev_date BETWEEN @wk_strt_date2 AND @wk_stop_date2))  --開發日期
+  AND (@wk_area_code = '' OR kc_area_code = @wk_area_code) --分公司
+  AND (@wk_biz_stat = '' OR kc_biz_stat = @wk_biz_stat) --登記現況
+  AND (@wk_sales_code2 = '' OR p.kc_sales_code = @wk_sales_code2) --業務人員
